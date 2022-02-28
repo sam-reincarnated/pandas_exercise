a08377a5182bc61e283ae51e69fff02c509dc3bf
Sameer Neelam <sam.reincarnated@gmail.com>
2022-03-01 00:20:20 +0530

    CSV file reading

--- lesson_01.py
+++ lesson_01.py
@@ -2,4 +2,15 @@ import pandas as pd
 # Loading data into pandas from multiple sources
 csv_filename = "./data/pokemon_data.csv"
 df = pd.read_csv(csv_filename)
-print(df.head(4))
+# Trying to print the whole data stream
+# print(df)
+# Trying to print the first N items in stream
+N = 3
+# print(df.head(N))
+# Trying to print the last N items in stream
+# print(df.tail(N))
+
+tsv_file = "./data/pokemon_data.txt"
+# Trying to read data from tab separated values
+df_tsv = pd.read_csv(tsv_file, delimiter='\t')
+# print(df_tsv.head(4))
